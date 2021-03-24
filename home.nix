1694000a599d5c8a69d87eec4d88ae6166f5c7b0
Michael Maurer <mjmaurer777@gmail.com>
2021-03-24 12:48:19 -0400

    don't sign by default and install scripts

--- home.nix
+++ home.nix
@@ -32,6 +32,7 @@
     shellAliases = {
       ".."  = "cd ..";
       "gc" = "git commit -v";
+      "gcs" = "git commit -v --gpg-sign";
       "ga" = "git add --all";
       "gs"  = "git status";
       "hig"  = "bat ~/.bash_history | grep";
@@ -51,10 +52,12 @@
     userEmail = "mjmaurer777@gmail.com";
     aliases = {
       pr = "pull --rebase";
+      gc = "commit -v";
+      gcs = "commit -v --gpg-sign";
     };
     signing = {
       key = "DA7297EEEF7B429CE7B4A11EE5DDBB38668F1E46";
-      signByDefault = true;
+      signByDefault = false;
     };
     extraConfig = {
       init.defaultBranch = "main";
---
+++ install.sh
@@ -0,0 +1,6 @@
+curl -L https://nixos.org/nix/install | sh
+mkdir ~/.config
+ln -s `pwd` ~/.config/nixpkgs
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
