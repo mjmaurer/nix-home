f54a303709b5dd9ebbf6d528ed2fcac6a6ee8cc1
Michael Maurer <mjmaurer777@gmail.com>
2023-03-23 10:44:45 -0400

    [fix] move to docker based

---
+++ homelab-proxy/homelab-proxy.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+let
+  proxyAddr = "howell.haus";
+in
+{
+  imports = [ ../common.nix ../common-linux.nix ];
+
+  home.username = pkgs.lib.mkForce "ubuntu";
+
+  home.packages = with pkgs; [
+    docker
+  ];
+
+  programs.bash.sessionVariables = {
+    MACHINE_NAME = "homelab-proxy";
+  };
+
+}
